59d13e407f6af497c226c7b33f69253a7426a4b1
Wojciech <janich9xy@gmail.com>
2022-06-24 22:14:47 +0200

    poruszniae przeciwnikow

--- Assets/Scripts/InvadersM.cs
+++ Assets/Scripts/InvadersM.cs
@@ -10,15 +10,63 @@ public class InvadersM : MonoBehaviour
 
     public int columns = 11;
 
+    private float speed = 1.0f;
+    private Vector3 _direction = Vector2.right;
 
     private void Awake()
     {
         for(int row = 0; row < this.rows; row++)
         {
+            float width = 2.0f * (this.columns - 1);
+            float height = 2.0f * (this.rows - 1);
+
+            Vector3 centering = new Vector2(-width / 2, -height / 2);
+
+            Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 2.0f), 0.0f);
+
             for(int col = 0; col < this.columns; col++)
             {
                Invaders invader = Instantiate(this.prefabs[row], this.transform);
+                Vector3 position = rowPosition;
+                position.x += col * 2.0f;
+                invader.transform.localPosition = position;
             }
         }
+    }
+
+
+    private void Update()
+    {
+        transform.position += _direction * speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in transform)
+        {
+            if (!invader.gameObject.activeInHierarchy) {
+                continue;
+            }
+
+            if (_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+            else if (_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+        }
+    }
+
+    private void AdvanceRow()
+    {
+        _direction = new Vector3(-_direction.x, 0f, 0f);
+
+        Vector3 position = transform.position;
+        position.y -= 1f;
+        transform.position = position;
     }
 }
